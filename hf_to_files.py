74a20903d51f3b072492ce45a5ee795518bdbd07
kingh0730 <46679373+kingh0730@users.noreply.github.com>
2023-07-08 17:50:26 -0700

    interesting

--- hf_to_files.py
+++ hf_to_files.py
@@ -14,23 +14,37 @@ ds = load_dataset(
     split=SPLIT,
 )
 
-for row in ds:
-    problem_id = row["problem_id"]
-    question = row["question"]
-    solutions = row["solutions"]
-    input_output = row["input_output"]
-    difficulty = row["difficulty"]
-    url = row["url"]
-    starter_code = row["starter_code"]
 
-    folder = f"./problem_{problem_id}"
+def main():
+    for row in ds:
+        problem_id = row["problem_id"]
+        question = row["question"]
+        solutions = row["solutions"]
+        input_output = row["input_output"]
+        difficulty = row["difficulty"]
+        url = row["url"]
+        starter_code = row["starter_code"]
 
-    os.makedirs(folder, exist_ok=True)
+        folder = f"./problem_{problem_id}"
 
-    with open(f"{folder}/question.txt", "w", encoding="utf-8") as f:
-        f.write(question)
-        f.write("\n")
+        os.makedirs(folder, exist_ok=True)
 
-    with open(f"{folder}/url.txt", "w", encoding="utf-8") as f:
-        f.write(url)
-        f.write("\n")
+        with open(f"{folder}/question.txt", "w", encoding="utf-8") as f:
+            f.write(question)
+            f.write("\n")
+
+        with open(f"{folder}/url.txt", "w", encoding="utf-8") as f:
+            f.write(url)
+            f.write("\n")
+
+        with open(f"{folder}/difficulty.txt", "w", encoding="utf-8") as f:
+            f.write(difficulty)
+            f.write("\n")
+
+        with open(f"{folder}/starter_code.py", "w", encoding="utf-8") as f:
+            f.write(starter_code)
+            f.write("\n")
+
+
+if __name__ == "__main__":
+    main()
