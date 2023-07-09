93ef68664b371de02166f1cf3c0a13c26f9872ed
kingh0730 <46679373+kingh0730@users.noreply.github.com>
2023-07-08 18:02:04 -0700

    works

--- hf_to_files.py
+++ hf_to_files.py
@@ -1,4 +1,5 @@
 import os
+import json
 from datasets import load_dataset
 
 
@@ -19,15 +20,17 @@ def main():
     for row in ds:
         problem_id = row["problem_id"]
         question = row["question"]
-        solutions = row["solutions"]
-        input_output = row["input_output"]
-        difficulty = row["difficulty"]
         url = row["url"]
+        difficulty = row["difficulty"]
         starter_code = row["starter_code"]
+        solutions = json.loads(row["solutions"])
+        input_output = json.loads(row["input_output"])
 
         folder = f"./problem_{problem_id}"
 
         os.makedirs(folder, exist_ok=True)
+        os.makedirs(f"{folder}/solutions", exist_ok=True)
+        os.makedirs(f"{folder}/input_output", exist_ok=True)
 
         with open(f"{folder}/question.txt", "w", encoding="utf-8") as f:
             f.write(question)
@@ -44,6 +47,17 @@ def main():
         with open(f"{folder}/starter_code.py", "w", encoding="utf-8") as f:
             f.write(starter_code)
             f.write("\n")
+
+        for i, solution in enumerate(solutions):
+            with open(
+                f"{folder}/solutions/solution_{i}.py", "w", encoding="utf-8"
+            ) as f:
+                f.write(solution)
+                f.write("\n")
+        for i, io in enumerate(input_output):
+            with open(f"{folder}/input_output/io_{i}.txt", "w", encoding="utf-8") as f:
+                f.write(io)
+                f.write("\n")
 
 
 if __name__ == "__main__":
